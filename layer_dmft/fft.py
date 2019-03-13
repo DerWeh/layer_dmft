8c16f93cf0ddca0b81ab39f21e8551e72023f836
Weh Andreas <andreas.weh@student.uni-augsburg.de>
2019-03-13 10:50:17 +0100

    Add Fourier transform iw->tau

---
+++ layer_dmft/fft.py
@@ -0,0 +1,64 @@
+"""Fourier transforms for Matsubara Green's functions for iω_n ↔ τ."""
+import numpy as np
+import gftools as gt
+
+
+def bare_dft_iw2tau(gf_iw, beta):
+    r"""Perform the discrete Fourier transform on the Hermitian function `gf_iw`.
+
+    Hermitian means here that the function producing `gf_iw` has to fulfill
+    :math:`f(-iω_n) = f^*(iω_n)`, thus the transform :math:`\tilde{f}(τ)` is
+    real.
+    It is assumed that the high frequency moments have been stripped from `gf_iw`,
+    else oscillations will appear.
+
+
+    Parameters
+    ----------
+    gf_iw : (..., N_iw) complex np.ndarray
+        The function at **fermionic** Matsubara frequencies.
+    beta : float
+        The inverse temperature.
+
+    Returns
+    -------
+    gf_tau_res : (..., 2*N_iw + 1) float np.ndarray
+        The Fourier transform of `gf_iw` on the interval [0, β].
+
+    """
+    shape = gf_iw.shape
+    N_tau = 2*shape[-1] + 1
+    gf_iw_paded = np.zeros(shape[:-1] + (N_tau,), dtype=gf_iw.dtype)
+    gf_iw_paded[..., 1:-1:2] = gf_iw
+    gf_tau = np.fft.hfft(gf_iw_paded/beta)
+    gf_tau_res = gf_tau[..., :N_tau]  # trim to [0, \beta]
+
+    return gf_tau_res
+
+
+def dft_iw2tau(gf_iw, beta, moment=1., dft_backend=bare_dft_iw2tau):
+    """DFT from iω to τ, needing the 1/iw tail `moment`.
+
+    Parameters
+    ----------
+    gf_iw : (..., N_iw) complex np.ndarray
+        The function at **fermionic** Matsubara frequencies.
+    beta : float
+        The inverse temperature.
+    moment : float
+        High frequency moment of `gf_iw`.
+    dft_backend : callable, optional
+        The function called to perform the Fourier transform on the data stripped
+        off its high frequency moment.
+
+    Returns
+    -------
+    gf_tau : (..., 2*N_iw + 1) float np.ndarray
+        The Fourier transform of `gf_iw` on the interval [0, β].
+
+    """
+    iws = gt.matsubara_frequencies(np.arange(gf_iw.shape[-1]), beta=beta)
+    gf_iw = gf_iw - moment/iws
+    gf_tau = dft_backend(gf_iw, beta)
+    gf_tau = gf_tau - .5*moment
+    return gf_tau
