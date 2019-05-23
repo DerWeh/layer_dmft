e6725d1a42b6d8f056841f075cccb65c6da4b7f1
Weh Andreas <andreas.weh@student.uni-augsburg.de>
2019-05-23 09:51:31 +0200

    Plot errors for impurity data if available

--- plot/impurity_data.py
+++ plot/impurity_data.py
@@ -4,6 +4,17 @@ import matplotlib.pyplot as plt
 
 from layer_dmft import dataio
 
+
+def plot(data, data_err=None, axes=None, **kwds):
+    default_args = {'linestyle': '--', 'marker': 'x'}
+    if axes is None:
+        axes = plt.gca()
+    if data_err is None:
+        axes.plot(data, **kwds, **default_args)
+    else:
+        axes.errorbar(np.arange(data.size), data, yerr=data_err,
+                      **kwds, **default_args)
+
 it = None
 key = 'self_energy_iw'
 layer = 0
@@ -18,10 +29,15 @@ try:
     data = np.array([imp_lay[key] for imp_lay in imp_data.values()])
 except KeyError:
     # FIXME
-    print(f"'{key}' not in impurity data."
-          f"available keys:\n{tuple(imp_data.values())[0].keys()}")
+    print(f"'{key}' not in impurity data. "
+          f"available keys:\n{tuple(tuple(imp_data.values())[0].keys())}")
     raise SystemExit()
+try:
+    data_err = np.array([imp_lay[key + '_err'] for imp_lay in imp_data.values()])
+except KeyError:
+    data_err = np.zeros_like(data, dtype=bool)
 data = data[layer:layer+1] if layer is not None else data
+data_err = data_err[layer:layer+1] if layer is not None else data_err
 
 COMPLEX = True if np.any(np.iscomplex(data)) else False
 SPIN_DEP = True if data.ndim > 2 and data.shape[1] == 2 else False
@@ -33,23 +49,23 @@ axes = axes[:, 0]
 
 
 
-for dat_lay in data:
+for dat_lay, dat_lay_err in zip(data, data_err):
     if SPIN_DEP:
         if COMPLEX:
-            axes[0].plot(dat_lay[0].imag, '--x', label='Im up')
-            axes[0].plot(dat_lay[1].imag,  '--x',label='Im dn')
-            axes[1].plot(dat_lay[0].real, '--x', label='Re up')
-            axes[1].plot(dat_lay[1].real, '--x', label='Re dn')
+            plot(dat_lay[0].imag, data_err=dat_lay_err[0], label='Im up', axes=axes[0])
+            plot(dat_lay[1].imag, data_err=dat_lay_err[1], label='Im dn', axes=axes[0])
+            plot(dat_lay[0].real, data_err=dat_lay_err[0], label='Re up', axes=axes[1])
+            plot(dat_lay[1].real, data_err=dat_lay_err[1], label='Re dn', axes=axes[1])
         else:
-            plt.plot(dat_lay[0], '--x', label='up')
-            plt.plot(dat_lay[1], '--x', label='dn')
+            plot(dat_lay[0], data_err=dat_lay_err[0], label='up')
+            plot(dat_lay[1], data_err=dat_lay_err[1], label='dn')
     else:
         if COMPLEX:
-            axes[0].plot(dat_lay.imag)
-            axes[1].plot(dat_lay.real, '--')
+            plot(dat_lay.imag, data_err=dat_lay_err, axes=axes[0])
+            plot(dat_lay.real, data_err=dat_lay_err, axes=axes[1])
         else:
-            plt.plot(dat_lay, '--x')
-            plt.plot(dat_lay, '--x')
+            plot(dat_lay, data_err=dat_lay_err)
+            plot(dat_lay, data_err=dat_lay_err)
 
 
 for ax in axes:
